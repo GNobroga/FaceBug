efed9a9f5a71a09acaddd638aeda9dc68f13552f
Gabiroba <gabrielcardosogirarde@gmail.com>
2024-01-26 16:47:18 -0300

    [UPDATE] Criando path alias

--- src/app/modules/principal/principal.module.ts
+++ src/app/modules/principal/principal.module.ts
@@ -1,22 +1,23 @@
 import { NgModule } from '@angular/core';
-import { CommentBoxComponent } from './components/comment-box/comment-box.component';
-import { ContactComponent } from './components/contact/contact.component';
-import { ContactsComponent } from './components/contacts/contacts.component';
-import { ContentComponent } from './components/content/content.component';
-import { CreatePostComponent } from './components/create-post/create-post.component';
-import { FeedComponent } from './components/feed/feed.component';
-import { FeedItemComponent } from './components/feed-item/feed-item.component';
-import { GroupComponent } from './components/group/group.component';
-import { GroupsComponent } from './components/groups/groups.component';
-import { HeaderComponent } from './components/header/header.component';
-import { HeaderMenuComponent } from './components/header-menu/header-menu.component.';
-import NavItemComponent from './components/nav-item/nav-item.component';
-import NavListComponent from './components/nav-list/nav-list.component';
-import { PostComponent } from './components/post/post.component';
-import { PostsComponent } from './components/posts/posts.component';
-import { SidebarComponent } from './components/sidebar/sidebar.component';
-import SidebarItem from './components/sidebar-item/sidebar-item.component';
-import SidebarListComponent from './components/sidebar-list/sidebar-list.component';
+import { CommentBoxComponent } from '@principal-components/comment-box/comment-box.component';
+import { ContactComponent } from '@principal-components/contact/contact.component';
+import { ContactsComponent } from '@principal-components/contacts/contacts.component';
+import { ContentComponent } from '@principal-components/content/content.component';
+import { CreatePostComponent } from '@principal-components/create-post/create-post.component';
+import { FeedItemComponent } from '@principal-components/feed-item/feed-item.component';
+import { FeedComponent } from '@principal-components/feed/feed.component';
+import { GroupComponent } from '@principal-components/group/group.component';
+import { GroupsComponent } from '@principal-components/groups/groups.component';
+import { HeaderMenuComponent } from '@principal-components/header-menu/header-menu.component.';
+import { HeaderComponent } from '@principal-components/header/header.component';
+import NavItemComponent from '@principal-components/nav-item/nav-item.component';
+import NavListComponent from '@principal-components/nav-list/nav-list.component';
+import { PostComponent } from '@principal-components/post/post.component';
+import { PostsComponent } from '@principal-components/posts/posts.component';
+import SidebarItem from '@principal-components/sidebar-item/sidebar-item.component';
+import SidebarListComponent from '@principal-components/sidebar-list/sidebar-list.component';
+import { SidebarComponent } from '@principal-components/sidebar/sidebar.component';
+
 
 @NgModule({
   declarations: [
